340bfe3f34eb7a74dcace4e2ac9e70959ec93703
Ryna <pinktulips@Hotmail.com>
2021-08-09 10:06:54 -0700

    tag posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-  
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :name, uniqueness: true
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
